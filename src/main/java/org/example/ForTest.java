736d13c11e05fcd8105bcee89da6208855ee8917
Baksati <baksgc@gmail.com>
2024-01-15 23:53:01 +0300

    ForTest

---
+++ src/main/java/org/example/ForTest.java
@@ -0,0 +1,11 @@
+/*
+Демонстрация работы цикла for.
+Назовите этот файл ForTest.java.
+ */
+class ForTest {
+    public static void main (String[] args) {
+        int x;
+        for(x=0; x<10; x=x+1)
+            System.out.println("Значение x: "+ x);
+    }
+}
