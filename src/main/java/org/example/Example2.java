38edf0ff454d8c8ea481fd30fbed18ae7eef639e
Baksati <baksgc@gmail.com>
2024-01-15 23:14:32 +0300

    Example2

---
+++ src/main/java/org/example/Example2.java
@@ -0,0 +1,14 @@
+/*
+Еще один короткий пример.
+Назовите этот файл Example2.java.
+ */
+class Example2 {
+    public static void main (String[] args) {
+        int num; // Объявление переменной по имени num
+        num=100; // Присвоение переменной num значения 100
+        System.out.println("Значение num " + num);
+        num=num * 2;
+        System.out.print("Значение num * 2: ");
+        System.out.println(num);
+    }
+}
