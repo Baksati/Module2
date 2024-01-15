0247b9b8f85c2d3bde472fe5f39df7ca7a74383f
Baksati <baksgc@gmail.com>
2024-01-15 22:46:56 +0300

    Java

---
+++ src/main/java/org/example/Java.java
@@ -0,0 +1,10 @@
+/*
+Простая программа на Java.
+Назовите этот файл Example.java.
+ */
+class Example {
+    // Программа начинается с вызова main().
+    public static void main (String[] args) {
+        System.out.println("Простая программа на языке Java.");
+    }
+}
