582585cd28003fc9a0bdda5712e4d38e5caf329b
Baksati <baksgc@gmail.com>
2024-01-18 21:59:12 +0300

    Area

---
+++ src/main/java/org/example/Area.java
@@ -0,0 +1,10 @@
+// Вычислить площадь круга
+class Area {
+    public static void main (String[] args) {
+        double pi, r, a;
+        r=10.8; // радиус круга
+        pi=3.1416; // приближенное число pi
+        a=pi*r*r; // вычислить площадь круга
+        System.out.println("Площадь круга равна"+a);
+    }
+}
