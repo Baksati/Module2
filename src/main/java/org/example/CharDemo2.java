d8e9b9846f2016aaa058cbf3a5b30e3b810a7dcd
Baksati <baksgc@gmail.com>
2024-01-18 22:13:23 +0300

    CharDemo2

---
+++ src/main/java/org/example/CharDemo2.java
@@ -0,0 +1,10 @@
+// Переменные char ведут себя подобно целым числам.
+class CharDemo2 {
+    public static void main (String[] args) {
+        char chl;
+        chl='X';
+        System.out.println("chl содержит"+chl);
+        chl++; // инкрементировать chl
+        System.out.println("chl теперь содержит"+chl);
+    }
+}
