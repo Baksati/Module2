5024e4b1688a423312b92292064a3c1b3f851cd6
Baksati <baksgc@gmail.com>
2024-01-18 22:07:04 +0300

    CharDemo

---
+++ src/main/java/org/example/CharDemo.java
@@ -0,0 +1,11 @@
+// Демонстрация использовани типа данных char.
+class CharDemo {
+    public static void main (String[] args) {
+        char chl, ch2;
+        chl=88; // код для X
+        ch2='Y';
+        System.out.print("chl и ch2:");
+        System.out.println(chl+""+ch2);
+    }
+
+}
