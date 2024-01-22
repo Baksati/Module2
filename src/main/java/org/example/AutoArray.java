dd173b29a5d0dfe90bb8e5bd5626617f24dddd3b
Baksati <baksgc@gmail.com>
2024-01-22 23:05:25 +0300

    AutoArray

---
+++ src/main/java/org/example/AutoArray.java
@@ -0,0 +1,8 @@
+// Улучшенная версия предыдущей программы.
+class AutoArray {
+    public static void main (String[] args) {
+        int[] month_days={31,28,31,30,31,30,
+                          31,31,30,31,30,31};
+        System.out.println("В апреле"+month_days[3]+"дней.");
+    }
+}
