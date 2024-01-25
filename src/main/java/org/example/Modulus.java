f7d831f8e7da4ef41644c5e6f1b922eb296e7002
baksati <baksgc@gmail.com>
2024-01-25 21:39:03 +0300

    Modulus

---
+++ src/main/java/org/example/Modulus.java
@@ -0,0 +1,9 @@
+// Демонстрация работы операции %
+class Modulus {
+    public static void main (String[] args) {
+        int x=42;
+        double y=42.25;
+        System.out.println("x по модулю 10="+x%10);
+        System.out.println("y по модулю 10="+y%10);
+    }
+}
