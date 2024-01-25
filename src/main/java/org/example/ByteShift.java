9e91d47510c646ac9d7e99ff070f752b903ed654
baksati <baksgc@gmail.com>
2024-01-25 23:12:13 +0300

    ByteShift

---
+++ src/main/java/org/example/ByteShift.java
@@ -0,0 +1,12 @@
+// Сдвиг влево значения byte.
+class ByteShift {
+    public static void main (String[] args) {
+        byte a=64, b;
+        int i;
+
+        i=a<<2;
+        b=(byte) (a<<2);
+        System.out.println("Первоначальное значение a:"+a);
+        System.out.println("i и b:"+i+""+b);
+    }
+}
