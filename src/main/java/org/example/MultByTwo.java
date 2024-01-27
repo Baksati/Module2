0d79291bec40ef56d9d65151f735ec8ecc98df92
Baksati <baksgc@gmail.com>
2024-01-27 13:26:48 +0300

    MultByTwo

---
+++ src/main/java/org/example/MultByTwo.java
@@ -0,0 +1,11 @@
+// Сдвиг влево как быстрый способ умножения на 2.
+class MultByTwo {
+    public static void main (String [] args) {
+        int i;
+        int num=0xFFFFFFE;
+        for (i=0; i<4; i++) {
+            num=num<<1;
+            System.out.println(num);
+        }
+    }
+}
