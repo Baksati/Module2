abbf8976ec94342691665063458a199cc2dcbb66
baksati <baksgc@gmail.com>
2024-01-29 13:29:07 +0300

    SampleSwitch

---
+++ src/main/java/org/example/SampleSwitch.java
@@ -0,0 +1,21 @@
+// Простой пример применения оператора switch.
+class SampleSwitch {
+    public static void main (String[]args) {
+        for (int i=0;i<6;i++)
+        switch (i) {
+            case 0:
+                System.out.println("i равно нулю");
+                break;
+            case 1:
+                System.out.println("i равно единице");
+                break;
+            case 2:
+                System.out.println("i равно двум");
+                break;
+            case 3:
+                System.out.println("i равно трем");
+            default:
+                System.out.println("i больше трех");
+        }
+    }
+}
