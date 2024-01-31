5e7f6bd0e174cd5dfdd2fa461c47709123eaa84e
baksati <baksgc@gmail.com>
2024-01-31 21:47:50 +0300

    While

---
+++ src/main/java/org/example/While.java
@@ -0,0 +1,11 @@
+// Демонстрация работы цикла while.
+class While {
+    public static void main (String[] args) {
+        int n=10;
+
+        while (n > 0) {
+            System.out.println("Импульс номер"+n);
+            n--;
+        }
+    }
+}
