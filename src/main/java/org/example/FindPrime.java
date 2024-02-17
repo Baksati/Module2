4e2ff2447f93adcc5b7d6d71c1f280b1addb6d21
baksati <baksgc@gmail.com>
2024-02-17 16:57:11 +0300

    FindPrime

---
+++ src/main/java/org/example/FindPrime.java
@@ -0,0 +1,20 @@
+// Проверка, является ли число простым.
+class FindPrime {
+    public static void main(String[] args) {
+        int num;
+        boolean isPrime;
+
+        num = 14;
+        if (num < 2) isPrime = false;
+        else isPrime = true;
+
+        for (int i = 2; i <= num/i; i++) {
+            if ((num % i)==0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) System.out.println("является простым");
+        else System.out.println("не является простым");
+    }
+}
