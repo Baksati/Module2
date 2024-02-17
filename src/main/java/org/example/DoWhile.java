c0d30eed172060c931e8339f5654115c62430a7f
baksati <baksgc@gmail.com>
2024-02-17 14:32:14 +0300

    DoWhile

---
+++ src/main/java/org/example/DoWhile.java
@@ -0,0 +1,11 @@
+// Демонстрация работы цикла do-while.
+class DoWhile {
+    public static void main(String[] args) {
+        int n=10;
+
+        do {
+            System.out.println("Импульс номер" + n);
+            n--;
+        } while (n>10);
+    }
+}
