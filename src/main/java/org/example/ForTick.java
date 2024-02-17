cdf725b67a94ee2ed893ffff320b7e69d726e6c8
baksati <baksgc@gmail.com>
2024-02-17 16:49:19 +0300

    ForTick

---
+++ src/main/java/org/example/ForTick.java
@@ -0,0 +1,8 @@
+// Демонстрация работы цикла for.
+class ForTick {
+    public static void main(String[] args) {
+        int n;
+        for(n = 10; n > 0; n--)
+            System.out.println("Импульск номер"+n);
+    }
+}
