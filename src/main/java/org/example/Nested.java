caa8c209f0909b2c83a539e4715979ae52cbac6f
baksati <baksgc@gmail.com>
2024-02-18 13:37:02 +0300

    Nested

---
+++ src/main/java/org/example/Nested.java
@@ -0,0 +1,12 @@
+// Циклы могут быть вложенными.
+public class Nested {
+    public static void main(String[] args) {
+        int i, j;
+
+        for (i = 0; i < 10; i++) {
+            for (j = i; j < 10; j++)
+                System.out.println(".");
+                System.out.println();
+        }
+    }
+}
