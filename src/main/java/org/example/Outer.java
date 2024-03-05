bd707b7940f41e6d52544a5268db4e35107c3815
baksati <baksgc@gmail.com>
2024-03-05 23:08:11 +0300

    InnerClassDemo

--- src/main/java/org/example/Outer.java
+++ src/main/java/org/example/Outer.java
@@ -3,13 +3,14 @@ package org.example;
 public class Outer {
     int outer_x = 100;
     void test() {
-        Inner inner = new Inner();
-        inner.display();
-    }
-    // Внутрениий класс.
-    class Inner {
-        void display() {
-            System.out.println("display() : outer_x =" + outer_x);
+        for(int i = 0; i < 10; i ++) {
+            class Inner {
+                void display() {
+                    System.out.println("display() : outer_x =" + outer_x);
+                }
+            }
+            Inner inner = new Inner();
+            inner.display();
         }
     }
 }
