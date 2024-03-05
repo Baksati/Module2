08467d8a0202859e0e72e6c520baa3564aeac430
baksati <baksgc@gmail.com>
2024-03-05 22:58:48 +0300

    InnerClassDemo

---
+++ src/main/java/org/example/InnerClassDemo.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class InnerClassDemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        outer.test();
+    }
+}
---
+++ src/main/java/org/example/Outer.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Outer {
+    int outer_x = 100;
+    void test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+    // Внутрениий класс.
+    class Inner {
+        void display() {
+            System.out.println("display() : outer_x =" + outer_x);
+        }
+    }
+}
