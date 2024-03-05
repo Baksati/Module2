e0956b410db31a04d5ab4ffee02c16b6033a9ee8
baksati <baksgc@gmail.com>
2024-03-05 22:20:03 +0300

    StaticByName

---
+++ src/main/java/org/example/StaticByName.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class StaticByName {
+    public static void main(String[] args) {
+        StaticDemo.callme();
+        System.out.println("b =" + StaticDemo.b);
+    }
+}
