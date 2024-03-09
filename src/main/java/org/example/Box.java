cb1c84b74a13375a00ce7e0a40d6b395f41a01f0
baksati <baksgc@gmail.com>
2024-03-09 21:04:08 +0300

    DemoBoxWeight

--- src/main/java/org/example/Box.java
+++ src/main/java/org/example/Box.java
@@ -1,11 +1,16 @@
 package org.example;
-/* Здесь в классе Box определенны три конструктора для инициализации
-размеров объекта коробки различными способами.
- */
+// В этой программе используется наследование для расширения класса Box.
 public class Box {
     double width;
     double height;
     double depth;
+
+    // Конструктор, применяемый для клонирования объекта.
+    Box(Box ob) { // передать объект конструктору
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
 
     // Конструктор, используемый в случае указания всех размеров.
     Box(double w, double h, double d) {
@@ -13,18 +18,34 @@ public class Box {
         height = h;
         depth = d;
     }
+
     // Конструктор, применяемый в случае, если размеры вообще не указаны.
     Box() {
-        width = - 1; // использовать - 1 для обозначения.
-        height = - 1; // неинициализированного.
-        depth = - 1; // объект коробки.
+        width = - 1; // использовать - 1 для обозначения
+        height = - 1; // неинициализированного
+        depth = - 1; // объекта коробки
     }
-    // Конструктор, используемый в случае создания объекта кубика.
+
+    // Конструктор, использованный в случае создания объекта кубической коробки.
     Box(double len) {
         width = height = depth = len;
     }
+
     // Вычислить и возвратить объем.
     double volume() {
         return width * height * depth;
+    }
+}
+
+// Здесь класс Box расширяется с целью включения члена weight.
+class BoxWeight extends Box {
+    double weight; // вес коробки
+
+    // Конструктор для BoxWeight.
+    BoxWeight(double w, double h, double d, double m) {
+        width = w;
+        height = h;
+        depth = d;
+        weight = m;
     }
 }
