527f5f0743ef1c312f6be50342be9367586341eb
baksati <baksgc@gmail.com>
2024-03-16 23:36:01 +0300

    AskMe

---
+++ src/main/java/org/example/AskMe.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class AskMe implements SharedConstans{
+    static void answer(int result) {
+        switch (result) {
+            case NO:
+                System.out.println("Нет");
+                break;
+            case YES:
+                System.out.println("Да");
+                break;
+            case MAYBE:
+                System.out.println("Возможно");
+                break;
+            case LATER:
+                System.out.println("Позже");
+                break;
+            case SOON:
+                System.out.println("Скоро");
+                break;
+            case NEVER:
+                System.out.println("Никогда");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Question q = new Question();
+
+        answer(q.ask());
+        answer(q.ask());
+        answer(q.ask());
+        answer(q.ask());
+    }
+}
---
+++ src/main/java/org/example/SharedConstans.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public interface SharedConstans {
+    int NO = 0;
+    int YES = 1;
+    int MAYBE = 2;
+    int LATER = 3;
+    int SOON = 4;
+    int NEVER = 5;
+}
