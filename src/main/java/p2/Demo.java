0510ef6718a8da6b95513cd4257e01396d5c3fe7
baksati <baksgc@gmail.com>
2024-03-16 16:48:55 +0300

    Demo

---
+++ src/main/java/p2/Demo.java
@@ -0,0 +1,9 @@
+// Тестирование пакета p2.
+package p2;
+// Создать экземпляры различных классов p2.
+public class Demo {
+    public static void main(String[] args) {
+        Protection2 ob1 = new Protection2();
+        OtherPackage ob2 = new OtherPackage();
+    }
+}
