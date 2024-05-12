1423fc3e406af8e3027fb018ab890ac5501e939e
baksati <baksgc@gmail.com>
2024-05-12 18:09:31 +0300

    HashSetDemo

---
+++ src/main/java/org/example/HashSetDemo.java
@@ -0,0 +1,22 @@
+// Демонстрация работы HashSet.
+package org.example;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+
+        // Создать хеш-таблицу.
+        HashSet<String> hs = new HashSet<>();
+
+        // Добавить элементы в хеш-таблицу.
+        hs.add("Beta");
+        hs.add("Alpha");
+        hs.add("Eta");
+        hs.add("Gamma");
+        hs.add("Epsilon");
+        hs.add("Omega");
+
+        System.out.println(hs);
+    }
+}
