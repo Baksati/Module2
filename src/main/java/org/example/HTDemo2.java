2d7037e93eb66e4e12cb3d1b2d9fa3163acc0a2c
baksati <baksgc@gmail.com>
2024-05-31 22:45:25 +0300

    HTDemo2

---
+++ src/main/java/org/example/HTDemo2.java
@@ -0,0 +1,38 @@
+// Использование итераторов с Hashtable.
+package org.example;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HTDemo2 {
+    public static void main(String[] args) {
+        Hashtable<String, Double> balance = new Hashtable<>();
+
+        String str;
+        double bal;
+
+        balance.put("John Doe", 3434.34);
+        balance.put("Tom Smith", 123.22);
+        balance.put("Jane Baker", 1378.00);
+        balance.put("Tod Hall", 99.22);
+        balance.put("Ralph Smith", -19.08);
+
+        // Отобразить балансы всех счетов из хеш-таблицы.
+        // Для начала получить представление в виде набора ключей.
+        Set<String> set = balance.keySet();
+
+        // Получить итератор.
+        Iterator<String> itr = set.iterator();
+        while (itr.hasNext()) {
+            str = itr.next();
+            System.out.println(str + " : " + balance.get(str));
+        }
+        System.out.println();
+
+        // Пополнить счет клиента John Doe на 1000.
+        bal = balance.get("John Doe");
+        balance.put("John Doe", bal + 1000);
+        System.out.println("Новый баланс клиента John Doe: " + balance.get("John Doe"));
+    }
+}
