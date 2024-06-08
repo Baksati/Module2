39fef3fa379208d16ca9b4795e311e61c92cf212
baksati <baksgc@gmail.com>
2024-06-08 20:06:46 +0300

    Find

--- src/main/java/org/example/Find.java
+++ src/main/java/org/example/Find.java
@@ -13,10 +13,18 @@ public class Find {
         }
         return false;
     }
-
+    
     public static void main(String[] args) {
         int[] array1 = {4, 5, 6, 6, 8};
         System.out.println(containsDuplicate(array1));
+
+        array1 = new int[]{4, 5, 6, 6, 8};
+        int previous = 6;
+        for(int nums : array1) {
+            if(previous == nums) {
+                System.out.println(nums);
+            }
+        }
 
         int[] array2 = {4, 5, 6, 7, 8};
         System.out.println(containsDuplicate(array2));
