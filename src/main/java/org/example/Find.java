384314c92631fe49a1ac572fb0c02ee3cbdcc36c
baksati <baksgc@gmail.com>
2024-06-09 14:30:45 +0300

    Find

--- src/main/java/org/example/Find.java
+++ src/main/java/org/example/Find.java
@@ -1,30 +1,36 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Find {
-    public static boolean containsDuplicate(int[] array) {
-        Set<Integer> set = new HashSet<>(); //
-        for(int nums : array) {
-            return true;
-        }
-        return false;
-    }
-    
     public static void main(String[] args) {
-        int[] array1 = {4, 5, 6, 6, 8};
+        int[] array1 = {4, 5, 6, 6, 8, 9};
         System.out.println(containsDuplicate(array1));
+//        int[] array2 = {4, 5, 6, 7, 8};
+//        containsDuplicate(array2);
 
-        array1 = new int[]{4, 5, 6, 6, 8};
-        int previous = 6;
-        for(int nums : array1) {
-            if(previous == nums) {
-                System.out.println(nums);
-            }
-        }
+//        array1 = new int[]{4, 5, 6, 6, 8};
+//        int previous = 6;
+//        for(int nums : array1) {
+//            if(previous == nums) {
+//                System.out.println(nums);
+//            }
+//        }
 
-        int[] array2 = {4, 5, 6, 7, 8};
-        System.out.println(containsDuplicate(array2));
+
+    }
+
+    public static Set<Integer> containsDuplicate(int[] array) {
+        Set<Integer> set = new HashSet<>();
+        // TODO добавить все элементы вне зависимости от колл-ва
+        set.add(4);
+        set.add(5);
+        set.add(6);
+        set.add(6);
+        set.add(8);
+        set.add(9);
+        return set;
     }
 }
