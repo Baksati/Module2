8e8c9f9e7800876b3c8ef8d23e6744451fb195bc
baksati <baksgc@gmail.com>
2024-06-10 21:17:31 +0300

    Find

--- src/main/java/org/example/Find.java
+++ src/main/java/org/example/Find.java
@@ -6,8 +6,10 @@ public class Find {
     public static void main(String[] args) {
         int[] array1 = {4, 5, 6, 6, 8, 9};
         System.out.println(containsDuplicate(array1));
-  //      int[] array2 = {4, 5, 6, 7, 8};
-  //      containsDuplicate(array2);
+        int[] array2 = {4, 5, 6, 7, 8};
+        System.out.println(containsDuplicate(array2));
+
+
 
 //        array1 = new int[]{4, 5, 6, 6, 8};
 //        int previous = 6;
@@ -21,7 +23,9 @@ public class Find {
     }
 
     public static boolean containsDuplicate(int[] array1) {
-        List<Integer> numbers = List.of(4, 5, 6, 6, 8, 9);
+        List<Integer> numbers = new ArrayList<>();
+        for (int a: array1)
+            numbers.add(a);
         Set<Integer> set = new HashSet<>();
         List<Object> duplicates = new ArrayList<>();
         numbers.forEach(n -> {
@@ -31,6 +35,7 @@ public class Find {
                 });
         System.out.println("Элементы из списка" + numbers);
         System.out.println("Дублирующий элемент: " + duplicates);
-        return true;
+
+        return !duplicates.isEmpty();
     }
 }
