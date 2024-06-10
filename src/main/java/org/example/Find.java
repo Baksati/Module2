45040355beaf5748c8963fbf6443c81459be7acc
baksati <baksgc@gmail.com>
2024-06-10 20:25:17 +0300

    Find

--- src/main/java/org/example/Find.java
+++ src/main/java/org/example/Find.java
@@ -1,15 +1,13 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Find {
     public static void main(String[] args) {
         int[] array1 = {4, 5, 6, 6, 8, 9};
         System.out.println(containsDuplicate(array1));
-//        int[] array2 = {4, 5, 6, 7, 8};
-//        containsDuplicate(array2);
+        int[] array2 = {4, 5, 6, 7, 8};
+        containsDuplicate(array2);
 
 //        array1 = new int[]{4, 5, 6, 6, 8};
 //        int previous = 6;
@@ -23,14 +21,21 @@ public class Find {
     }
 
     public static Set<Integer> containsDuplicate(int[] array) {
+        List<Integer> numbers = List.of(4, 5, 6, 6, 8, 9);
+        List<Integer> numbers2 = List.of(4, 5, 6, 7, 8);
         Set<Integer> set = new HashSet<>();
-        // TODO добавить все элементы вне зависимости от колл-ва
-        set.add(4);
-        set.add(5);
-        set.add(6);
-        set.add(6);
-        set.add(8);
-        set.add(9);
+        List<Object> duplicates = new ArrayList<>();
+        numbers.forEach(n -> {
+            if (!set.add(n)) {
+                duplicates.add(n);
+            }
+                });
+        numbers2.forEach(n2 -> {
+            if (!set.add(n2)) {
+                duplicates.add(n2);
+            }
+        });
+        System.out.println("Duplicates elements: " + duplicates);
         return set;
     }
 }
