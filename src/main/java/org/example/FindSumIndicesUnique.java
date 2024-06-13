96b4bba6e794205f1d624a398a7eec3fc9ebbaf5
baksati <baksgc@gmail.com>
2024-06-13 22:36:35 +0300

    FindSumIndicesUnique

--- src/main/java/org/example/FindSumIndicesUnique.java
+++ src/main/java/org/example/FindSumIndicesUnique.java
@@ -4,24 +4,24 @@ import java.util.Arrays;
 
 public class FindSumIndicesUnique {
     public static void main(String[] args) {
-        int[] arr = {3, 8, 15, 17};
-        int sum = 23;
+        int[] array = {3, 8, 15, 17};
+        int number = 23;
 
-        int[] indices = FindSumIndicesUnique(arr, sum);
+        int[] indices = FindSumIndicesUnique(array, number);
 
         System.out.println(Arrays.toString(indices));
     }
-
-    public static int[] FindSumIndicesUnique(int[] arr, int sum) {
+    public static int[] FindSumIndicesUnique(int[] array, int sum) {
         int[] indices = new int[2];
-        for (int i = 0; i < arr.length; i++) {
-            int target = sum - arr[i];
 
-            int index = Arrays.binarySearch(arr, i + 1, arr.length, target);
+        for (int i = 0; i < array.length; i++) {
+            int target = sum - array[i];
 
-            if (index >= 0) {
-                indices[0] = arr [i];
-                indices[1] = arr [index];
+            int index = Arrays.binarySearch(array, i + 1, array.length, target);
+
+            if (index >=0) {
+                indices[0] = array [i];
+                indices[1] = array [index];
                 return indices;
             }
         }
