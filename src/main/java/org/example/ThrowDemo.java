4997f968ebcd2f7f37a30be69b7616f6c65f01de
Baksati <baksgc@gmail.com>
2024-07-10 22:16:19 +0300

    ThrowDemo

---
+++ src/main/java/org/example/ThrowDemo.java
@@ -0,0 +1,21 @@
+// Демонстрация применения throw.
+package org.example;
+
+public class ThrowDemo {
+    static void demoproc() {
+        try {
+            throw new NullPointerException("демонстрация");
+        } catch (NullPointerException e) {
+            System.out.println("Перехвачено внутри demoproc().");
+            throw e; // повторно сгенерировать исключение
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            demoproc();
+        } catch (NullPointerException e) {
+            System.out.println("Повторно перехвачено:" + e);
+        }
+    }
+}
