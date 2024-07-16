5f2bd03633a18653984292e7d74c8aaba7534b3d
Baksati <baksgc@gmail.com>
2024-07-16 20:20:52 +0300

    BRReadLines

--- src/main/java/org/example/BRReadLines.java
+++ src/main/java/org/example/BRReadLines.java
@@ -1,21 +1,23 @@
-// Чтение строки консолис применением BufferedReader.
+// Чтение строки консоли c применением BufferedReader.
 package org.example;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class BRReadLines {
     public static void main(String[] args) throws IOException {
 
         // Создать объект BufferedReader, используя System.in.
         BufferedReader br = new BufferedReader(new
-                InputStreamReader(System.in, System.console().charset()));
+                InputStreamReader(System.in));
 
-        char str;
+        char c;
         System.out.println("Вводите строки текста.");
         System.out.println("Для завершения введите stop.");
         do {
-            str = (char) br.read();
-            System.out.println(str);
-        } while (str != 'q');
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
     }
 }
---
+++ src/main/java/org/example/IOException.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class IOException extends Exception {
+}
