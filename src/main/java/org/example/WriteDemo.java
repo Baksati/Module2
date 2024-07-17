7d325bd67f2ed17b1e674e807e1c664a428f7754
Baksati <baksgc@gmail.com>
2024-07-18 00:04:40 +0300

    WriteDemo

---
+++ src/main/java/org/example/WriteDemo.java
@@ -0,0 +1,12 @@
+// Демонстрация использования System.out.write().
+package org.example;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int b;
+
+        b = 'A';
+        System.out.write(b);
+        System.out.write('\n');
+    }
+}
