6f694a542f73d3e335149642150d22fc20025d77
Baksati <baksgc@gmail.com>
2024-08-27 22:14:42 +0300

    ByteArrayInputStreamReset

---
+++ src/main/java/org/example/ByteArrayInputStreamReset.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.*;
+
+public class ByteArrayInputStreamReset {
+    public static void main(String[] args) {
+        String tmp = "abc";
+        byte [] b = tmp.getBytes();
+        ByteArrayInputStream in = new ByteArrayInputStream(b);
+
+        for (int i = 0; i < 2; i ++) {
+            int c;
+            while ((c = in.read()) != -1) {
+                if (i == 0) {
+                    System.out.println((char) c);
+                } else {
+                    System.out.println(Character.toUpperCase((char) c));
+                }
+            }
+            System.out.println();
+            in.reset();
+        }
+    }
+}
