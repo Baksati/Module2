fa027f67cc5b0723f0dc64d22b991e8ad318334c
Baksati <baksgc@gmail.com>
2024-09-01 23:41:58 +0300

    BufferedReaderDemo

---
+++ src/main/java/org/example/BufferedReaderDemo.java
@@ -0,0 +1,56 @@
+package org.example;
+
+// Использование буферезированного ввода.
+
+import java.io.*;
+import java.io.IOException;
+
+public class BufferedReaderDemo {
+    public static void main(String[] args) throws IOException {
+
+        String s = "Конструкция &copy; - символ авторского права," +
+                "но конструкция &copy - нет. \n";
+        char[] buf = new char[s.length()];
+        s.getChars(0, s.length(), buf, 0);
+
+        CharArrayReader in = new CharArrayReader(buf);
+        int c;
+        boolean marked = false;
+        try(BufferedReader f = new BufferedReader(in))
+        {
+            while ((c = f.read()) != -1) {
+                switch (c) {
+                    case '&' :
+                        if(!marked) {
+                            f.mark(32);
+                            marked = true;
+                        } else {
+                            marked = false;
+                        }
+                        break;
+                    case ';' :
+                        if(marked) {
+                            marked = false;
+                            System.out.print("(c)");
+                        } else
+                            System.out.print((char) c);
+                        break;
+                    case ' ' :
+                        if(marked) {
+                            marked = false;
+                            f.reset();
+                            System.out.print("&");
+                        } else
+                            System.out.print((char) c);
+                        break;
+                    default:
+                        if(!marked)
+                            System.out.print((char) c);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода-вывода: " + e);
+        }
+    }
+}
